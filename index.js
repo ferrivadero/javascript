29b1da6b966252c8affb1fea0fa1f05035fa8855
fer rivadero <fernandorivadero@gmail.com>
2022-06-22 19:41:09 -0300

    aaa

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let numeroCalificaciones = parseInt(prompt("Ingrese el numero de calificaciones"))
+let sumatoria = 0
+let promedio = 0
+
+for (let i = 1; i<= numeroCalificaciones; i++) {
+    let calificacion = parseFloat(prompt("Ingrese la calificacion " + i))
+    sumatoria = sumatoria + calificacion
+}
+
+promedio = sumatoria / numeroCalificaciones
+
+alert("El promedio es " + promedio)
+
+function ResultadoPromedio(){
+    if (promedio >= 7){
+        alert("Estado: Promocionado")
+    }
+    else if(promedio < 7 & promedio >= 4){
+        alert("Estado: Regular")
+    }
+    else{
+        alert("Estado: Libre")
+    }
+}
+
+ResultadoPromedio()
