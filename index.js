1abe1e0475e6ae9a287c800f717c8f9fe53811bc
fer rivadero <fernandorivadero@gmail.com>
2022-07-10 20:14:53 -0300

    se agregan eventos

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-const productos = [{id: 1, nombre: "colaless", precio: 500, categoria: "ropa interior"},
+/*const productos = [{id: 1, nombre: "colaless", precio: 500, categoria: "ropa interior"},
             {id: 2, nombre: "pijama", precio: 2500, categoria: "pijama"},
             {id: 3, nombre: "corpiño", precio: 700, categoria: "ropa interior"},
             {id: 4, nombre: "remera", precio: 1500, categoria: "ropa"},
@@ -37,4 +36,174 @@ function simCompra(){
 
 simCompra()
 
+*/
+class Producto {
+    constructor(id, nombre, precio, imagen) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.imagen = imagen
+    }
+}
 
+class Carrito {
+    constructor(id) {
+        this.id = id
+        this.productos = []
+    }
+
+    calcularTotal() {
+        let total = 0
+        for(let i = 0; i < this.productos.length; i++) {
+            total += this.productos[i].precio
+        }
+        return total
+    }
+}
+
+/* FUNCIONES */
+
+function individualTarjeta(producto) {
+    let tarjetaIndividual = ` 
+                <div class="card" >
+                <img src="img/${producto.imagen}" alt="">
+                <h5>${producto.nombre}</h5>
+                <p>$ ${producto.precio}</p>
+                <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carro</a>
+            </div>`;
+
+    return tarjetaIndividual;
+}
+
+function tarjetaCarro(producto) {
+    let carroTarjeta = `
+                        <hr><div class="tarjeta-carrito">
+                            <div class="imagen">
+                                <img src="img/${producto.imagen}" alt="">
+                            </div>
+                            <div >
+                                <h5>${producto.nombre}</h5>
+                            </div>
+                            <div>
+                                <h5>$ ${producto.precio}</h5>
+                            </div>
+                        </div><hr>`
+    return carroTarjeta 
+}
+
+function limpiarCarrito() {
+    let divCarrito = document.querySelector("#carro")
+    divCarrito.innerHTML = ""
+}
+
+function actualizarCarrito() {
+    let divCarrito = document.querySelector("#carro")
+    carrito.productos.forEach(producto => {
+        divCarrito.innerHTML += tarjetaCarro(producto)
+    })
+    divCarrito.innerHTML += `<h1>Total: $ ${carrito.calcularTotal()}</h1>`
+}
+
+function renovarStorage() {
+    localStorage.removeItem("carrito")
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+/* CARGAR CARRITO EXISTENTE */
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    let storage = JSON.parse(localStorage.getItem("carro"))
+    let carritoGuardado = new Carrito(storage.id, storage.productos)
+    storage.productos.forEach(producto => {
+        carritoGuardado.producto.push(producto)
+    })
+    limpiarCarrito()
+    actualizarCarrito(carritoGuardado)
+})
+
+/* GENERACION CATALOGO DE PRODUCTOS*/
+
+let Catalogo = []
+
+let producto1 = new Producto(1, "Remera", 2000, "modelo.jpg")
+let producto2 = new Producto(2, "Short", 1500, "short.jpg")
+let producto3 = new Producto(3, "Pijama", 3800, "pijama.jpg")
+let producto4 = new Producto(4, "Pantalon", 3500, "pantalon.jpg")
+
+
+
+Catalogo.push(producto1)
+Catalogo.push(producto2)
+Catalogo.push(producto3)
+Catalogo.push(producto4)
+
+/* GENERACION DE TARJETAS DE PRODUCTOS*/
+
+let tarjetasDiv = document.querySelector("#tarjetas")
+
+Catalogo.forEach(producto => {
+    tarjetasDiv.innerHTML += individualTarjeta(producto)
+})
+
+/* carrito De un producto */
+
+let carrito = new Carrito(1)
+
+/* botones */
+
+let botones = document.querySelectorAll(".botonDeCompra")
+
+let arrayDeBotones = Array.from(botones)
+
+arrayDeBotones.forEach(boton => {
+            boton.addEventListener("click", (e) => {
+                    let productoSeleccionado = Catalogo.find(producto => producto.id == e.target.id)
+                    carrito.productos.push(productoSeleccionado)
+                    limpiarCarrito()
+                    actualizarCarrito(carrito)
+                    renovarStorage()
+                })
+            })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*let botonUno = document.getElementById("btnRopaUno")
+        let botonDos = document.getElementById("btnRopaDos")
+        let botonTres = document.getElementById("btnRopaTres")
+        let botonCuatro = document.getElementById("btnRopaCuatro")
+
+        botonUno.addEventListener("click", () => {
+                alert("se agrego al carrito")})
+
+        botonDos.addEventListener("click", () => {
+                alert("se agrego al carrito")})
+
+        botonTres.addEventListener("click", () => {
+                alert("se agrego al carrito")})
+
+        botonCuatro.addEventListener("click", () => {
+                alert("se agrego al carrito")})
+                    
+        */
